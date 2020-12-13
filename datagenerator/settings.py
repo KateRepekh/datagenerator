879a36f505b7fbe93d48feec312339fe3accef35
KateRepekh <krepekh@gmail.com>
2020-12-13 18:02:19 +0200

    Turn off DEBUG

--- datagenerator/settings.py
+++ datagenerator/settings.py
@@ -28,7 +28,7 @@ APP_NAME = 'DataGenerator'
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
